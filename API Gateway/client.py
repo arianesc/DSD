83321c890b4f62e18fb4a38ca3d19bcdf11959a8
Ariane Silveira <arianesilveira97@hotmail.com>
2023-12-02 16:58:51 -0300

    cria cliente

---
+++ API Gateway/client.py
@@ -0,0 +1,29 @@
+import requests
+
+def consumir_servico1():
+    try:
+        response = requests.get('http://localhost:3000/servico1')
+        response.raise_for_status()  # Lança uma exceção em caso de erro HTTP
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print(f'Erro ao consumir o Serviço 1: {e}')
+        return None
+
+def consumir_servico2():
+    try:
+        response = requests.get('http://localhost:3000/servico2')
+        response.raise_for_status()  # Lança uma exceção em caso de erro HTTP
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print(f'Erro ao consumir o Serviço 2: {e}')
+        return None
+
+if __name__ == '__main__':
+    resultado_servico1 = consumir_servico1()
+    resultado_servico2 = consumir_servico2()
+
+    if resultado_servico1:
+        print('Resultado do Serviço 1:', resultado_servico1)
+
+    if resultado_servico2:
+        print('Resultado do Serviço 2:', resultado_servico2)
